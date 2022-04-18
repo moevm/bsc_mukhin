ea3930bb9a7409cd760153ad392b639d9e55e3b3
amukhin <bbyxW4vL8qNhqwkh>
2022-04-18 14:04:07 +0300

    del: useless values

--- helper.py
+++ helper.py
@@ -1,30 +1,8 @@
-import base64
 import dataclasses
 import os
 from datetime import datetime
 
-import requests
-from fastapi.security import HTTPBasic
-
 from repository import Repository
-
-security = HTTPBasic()
-
-
-def request_access_token(code: str) -> dict:
-    endpoint = '/oauth/token'
-    authorization = f"{os.environ['CLIENT_ID']}:{os.environ['CLIENT_SECRET']}"
-    secret_authorization = base64.b64encode(authorization.encode(encoding='utf-8')).decode('utf-8')
-    headers = {
-        'Authorization': f'Basic {secret_authorization}',
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
-    query_parameters = {
-        'code': code,
-        'grant_type': 'authorization_code',
-        'redirect_uri': os.environ['REDIRECT_URI']+'/auth',
-    }
-    return requests.post(url=zoom_url+endpoint, headers=headers, params=query_parameters, timeout=5).json()
 
 
 @dataclasses.dataclass
